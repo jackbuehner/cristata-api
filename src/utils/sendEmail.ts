89426e9a8b1cc7c82fa160e861b78aa9aa52a37e
Jack Buehner <github@jackbuehner.com>
2021-09-17 04:04:27 +0000

    create function to send emails via AWS SES

---
+++ src/utils/sendEmail.ts
@@ -0,0 +1,54 @@
+import aws from 'aws-sdk';
+import dotenv from 'dotenv';
+
+// load environmental variables
+dotenv.config();
+
+// set the region to us-east
+aws.config.update({
+  region: 'us-east-1',
+});
+
+// make ses available
+const ses = new aws.SES({ apiVersion: '2010-12-01' });
+
+/**
+ * Send an HTML email using Amazon SES.
+ * @param to email address of destination person
+ * @param subject subject of the email
+ * @param message HTML message of the email (not plain text)
+ * @param from email address of the sender; defaults to `EMAIL_DEFAULT_FROM` in environment variables
+ */
+function sendEmail(
+  to: string | string[],
+  subject: string,
+  message: string,
+  from = process.env.EMAIL_DEFAULT_FROM
+) {
+  const params = {
+    Destination: {
+      ToAddresses: typeof to === 'string' ? [to] : to,
+    },
+    Message: {
+      Body: {
+        Html: {
+          Charset: 'UTF-8',
+          Data: message,
+        },
+      },
+      Subject: {
+        Charset: 'UTF-8',
+        Data: subject,
+      },
+    },
+    ReturnPath: from,
+    Source: from,
+  };
+  ses.sendEmail(params, (err, data) => {
+    if (err) {
+      return console.error(err, err.stack);
+    }
+  });
+}
+
+export { sendEmail };
