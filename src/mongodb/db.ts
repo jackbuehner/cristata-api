14ea881b04bd73a13795ea48c50110df26bb09be
Jack Buehner <github@jackbuehner.com>
2021-10-29 00:27:44 +0000

    feat: create mongoose schemas and models from the config

--- src/mongodb/db.ts
+++ src/mongodb/db.ts
@@ -6,6 +6,7 @@ import './photos.model';
 import './settings.model';
 import './shorturl.model';
 import './flush.model';
+import { merge } from 'merge-anything';
 
 // destructure connection info from config
 const { username, password, host, database, options } = config.database.connection;
@@ -16,4 +17,43 @@ mongoose.connect(`mongodb+srv://${username}:${password}@${host}/${database}?${op
   useUnifiedTopology: true,
   useFindAndModify: false,
   useCreateIndex: true,
+});
+
+// schema fields to include in every collection
+const collectionSchemaFields = {
+  timestamps: {
+    created_at: { type: Date, required: true, default: new Date().toISOString() },
+    modified_at: { type: Date, required: true, default: new Date().toISOString() },
+    published_at: { type: Date, required: true, default: '0001-01-01T01:00:00.000+00:00' },
+    updated_at: { type: Date, required: true, default: '0001-01-01T01:00:00.000+00:00' },
+  },
+  people: {
+    created_by: { type: Number },
+    modified_by: { type: [Number] },
+    last_modified_by: { type: Number },
+    published_by: { type: [Number] },
+    last_published_by: { type: Number },
+    watching: { type: [Number] },
+  },
+  hidden: { type: Boolean, required: true, default: false },
+  locked: { type: Boolean, required: true, default: false },
+  history: [
+    {
+      type: { type: String, required: true },
+      user: { type: Number, required: true },
+      at: {
+        type: Date,
+        required: true,
+        default: new Date().toISOString(),
+      },
+    },
+  ],
+};
+
+// create the schema and model for each collection
+config.database.collections.forEach((collection) => {
+  // create the schema
+  const Schema = new mongoose.Schema(merge(collectionSchemaFields, collection.schemaFields));
+  // create the model based on the schema
+  mongoose.model(collection.name, Schema);
 });
