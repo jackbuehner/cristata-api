33ad7dcaf6ed080d57e2e99295c61263b812971b
Jack Buehner <github@jackbuehner.com>
2021-10-29 22:06:34 +0000

    feat: add permissions to db config

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,5 +1,6 @@
 import { IResolvers } from '@graphql-tools/utils';
 import dotenv from 'dotenv';
+import { GitHubTeamNodeID, GitHubUserID } from '../mongodb/db';
 
 // load environmental variables
 dotenv.config();
@@ -15,13 +16,49 @@ const database = {
   collections: [],
 };
 
+const Teams = {
+  ADMIN: 'MDQ6VGVhbTQ2NDI0MTc=',
+  ANY: 'any',
+};
+
+const Users = {
+  ANY: 0,
+};
+
 interface Collection {
   name: string;
   canPublish?: boolean;
   typeDefs: string;
   resolvers: IResolvers<unknown, Record<string, unknown>, Record<string, unknown>, unknown>;
   schemaFields: Record<string, unknown>;
+  permissions: (users: typeof Users, teams: typeof Teams) => CollectionPermissions;
 }
 
-export type { Collection };
-export { database };
+type CollectionPermissionsType = {
+  teams: GitHubTeamNodeID[];
+  users: GitHubUserID[];
+};
+
+type CollectionPermissionsActions =
+  | 'get'
+  | 'create'
+  | 'modify'
+  | 'hide'
+  | 'lock'
+  | 'watch'
+  | 'publish'
+  | 'delete';
+
+interface CollectionPermissions {
+  get: CollectionPermissionsType;
+  create: CollectionPermissionsType;
+  modify: CollectionPermissionsType;
+  hide: CollectionPermissionsType;
+  lock: CollectionPermissionsType;
+  watch: CollectionPermissionsType;
+  publish?: CollectionPermissionsType;
+  delete: CollectionPermissionsType;
+}
+
+export type { Collection, CollectionPermissions, CollectionPermissionsActions };
+export { database, Teams, Users };
