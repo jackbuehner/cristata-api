a3f51f1aa72e4f3442729717c5288927cd3beb35
Jack Buehner <github@jackbuehner.com>
2021-11-03 03:17:32 +0000

    feat(gql): support nested objects when specifying keys to keep when pruning a doc

--- src/config/collections/helpers/pruneDocs.ts
+++ src/config/collections/helpers/pruneDocs.ts
@@ -1,5 +1,7 @@
 import { merge } from 'merge-anything';
 import mongoose from 'mongoose';
+import { flattenObject } from '../../../utils/flattenObject';
+import { unflattenObject } from '../../../utils/unflattenObject';
 
 interface PruneDocs<T> {
   input: T;
@@ -8,7 +10,12 @@ interface PruneDocs<T> {
 
 function pruneDocs<T extends mongoose.Document[]>({ input, keep }: PruneDocs<T>): mongoose.Document[] {
   const pruned = input.map((obj) => {
-    const keepProps = keep.map((key) => ({ [key]: obj[key] }));
+    const flatObj = flattenObject(obj as unknown as { [key: string]: never });
+    const keepProps = keep.map((key) =>
+      unflattenObject({
+        [key]: key === '_id' ? new mongoose.Types.ObjectId(flatObj['_id.id']) : flatObj[key],
+      })
+    );
     return merge({}, ...keepProps) as mongoose.Document;
   });
   return pruned;
