cfac954e8ff67274d79ed1fd515c0217cd0b75ea
Jack Buehner <github@jackbuehner.com>
2021-11-09 01:30:54 +0000

    fix: prevent processing of infinitly nested objects

--- src/utils/flattenObject.ts
+++ src/utils/flattenObject.ts
@@ -1,3 +1,5 @@
+const PROHIBITED_KEYS = ['$__', '$op', '$init', '$__parent', '$isSingleNested'];
+
 /**
  * Flattens an object while retaining its roots.
  *
@@ -9,16 +11,26 @@
 function flattenObject(
   obj: { [key: string]: never },
   roots: string[] = [],
-  sep = '.'
+  sep = '.',
+  tier = 0,
+  maxTier = 10
 ): { [key: string]: never } {
+  // refuse to flatten if tier is greater than the maximum allowed tier
+  // (this is to prevent crashes if an object references itself in one of its properties)
+  if (tier > maxTier) return {};
+
   // store the flattened object
   const flattened: { [key: string]: never } = {};
 
   // iterate through each key
   Object.keys(obj).forEach((key) => {
+    // if the key is prohibited, do nothing
+    if (PROHIBITED_KEYS.includes(key)) {
+      null;
+    }
     // if the key is an object, flatten the object with this function
-    if (Object.prototype.toString.call(obj[key]) === '[object Object]') {
-      Object.assign(flattened, flattenObject(obj[key], [...roots, key], sep));
+    else if (Object.prototype.toString.call(obj[key]) === '[object Object]') {
+      Object.assign(flattened, flattenObject(obj[key], [...roots, key], sep, tier + 1));
     }
     // otherwise, add to flattened object
     else {
