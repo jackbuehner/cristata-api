eb3b59cbbbde3ee4bcd8cfcdd849bf9696e38010
Jack Buehner <github@jackbuehner.com>
2021-11-10 05:04:39 +0000

    fix: `article_id` schema field should be ObjectID instead of String

--- src/config/collections/photoRequests.ts
+++ src/config/collections/photoRequests.ts
@@ -166,7 +166,7 @@ const photoRequests: Collection = {
     },
     stage: { type: Number, default: Stage.NEW },
     versions: { type: {} },
-    article_id: { type: String, default: '' },
+    article_id: { type: mongoose.Types.ObjectId, default: '' },
   }),
   permissions: (Users, Teams) => ({
     get: { teams: [Teams.ANY], users: [] },
