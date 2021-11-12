4eea3479b55b25c4324c509735c8be58c79af22d
Jack Buehner <github@jackbuehner.com>
2021-11-12 21:55:03 +0000

    feat!: require auth for websocket connections

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,9 @@ import { wss } from './websocket';
 import url from 'url';
 import { app } from './app';
 import { apollo, apolloWSS } from './apollo';
+import { parseCookies } from './utils/parseCookies';
+import Keygrip from 'keygrip';
+import { allowedOrigins } from './middleware/cors';
 
 // configure the server
 const hocuspocus = Hocuspocus.configure({
@@ -12,23 +15,49 @@ const hocuspocus = Hocuspocus.configure({
 
   // use hocuspocus at '/hocupocus' and use wss at '/websocket'
   onUpgrade: async ({ request, socket, head }) => {
-    const pathname = url.parse(request.url).pathname;
+    try {
+      const pathname = url.parse(request.url).pathname;
+      const origin = request.headers.origin;
 
-    if (pathname.indexOf('/hocuspocus/') === 0) {
-      // allow hocuspocus websocket to continue if the path starts with '/hocuspocus/
-    } else if (pathname === '/websocket') {
-      // use the wss websocket if the path is '/websocket
-      wss.handleUpgrade(request, socket, head, (ws) => {
-        wss.emit('connection', ws, request);
-      });
-    } else if (pathname === '/v3') {
-      // handle apollo subscriptions if the path is '/v3'
-      apolloWSS.handleUpgrade(request, socket, head, (ws) => {
-        apolloWSS.emit('connection', ws, request);
-      });
-    } else {
+      // ensure request is from a allowed origin
+      if (allowedOrigins.includes(origin) === false) {
+        throw new Error(`${origin} is not allowed to access websockets`);
+      }
+
+      // find auth cookie
+      const parsedCookies = parseCookies(request.headers.cookie);
+      const { name, value, signature } = parsedCookies.find((cookie) => cookie.name === 'github-auth-session');
+
+      // verify cookie integrity
+      const keygrip = new Keygrip([process.env.COOKIE_SESSION_SECRET]);
+      const isUntampered = keygrip.verify(`${name}=${value}`, signature);
+
+      // if the cookie has been modified by the client, end the connection
+      if (!isUntampered) {
+        throw new Error(`session cookie has been tampered with by the client`);
+      }
+
+      // if the cookie is untampered, use the following appropriate handlers
+      if (pathname.indexOf('/hocuspocus/') === 0) {
+        // allow hocuspocus websocket to continue if the path starts with '/hocuspocus/
+      } else if (pathname === '/websocket') {
+        // use the wss websocket if the path is '/websocket
+        wss.handleUpgrade(request, socket, head, (ws) => {
+          wss.emit('connection', ws, request);
+        });
+      } else if (pathname === '/v3') {
+        // handle apollo subscriptions
+        apolloWSS.handleUpgrade(request, socket, head, (ws) => {
+          apolloWSS.emit('connection', ws, request);
+        });
+      }
       // otherwise, end the websocket connection request
+      else {
+        socket.end();
+      }
+    } catch (error) {
       socket.end();
+      console.error(error);
     }
   },
 
