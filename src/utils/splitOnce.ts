f0ee09a9590d526f08c38974bf1e13a8b61b820e
Jack Buehner <github@jackbuehner.com>
2021-11-12 21:50:07 +0000

    feat: function to split string only once
    Useful for spliting string into only two parts

---
+++ src/utils/splitOnce.ts
@@ -0,0 +1,9 @@
+/**
+ * Splits a string by a separator once.
+ */
+function splitOnce(str: string, sep: string): [string, string] {
+  const [first, ...rest] = str.split(sep);
+  return [first, rest.length > 0 ? rest.join(sep) : null];
+}
+
+export { splitOnce };
