583552170dca22e53f8ff729da5bb4871a9d37c5
Jack Buehner <github@jackbuehner.com>
2021-12-07 17:30:05 +0000

    feat: enable optional filters on findDoc helper

--- src/config/collections/helpers/findDoc.ts
+++ src/config/collections/helpers/findDoc.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { Context } from '../../../apollo';
-import mongoose from 'mongoose';
+import mongoose, { FilterQuery } from 'mongoose';
 import { Teams } from '../../database';
 import { CollectionDoc, requireAuthentication } from '.';
 
@@ -8,11 +8,12 @@ interface FindDoc {
   model: string;
   by?: string;
   _id: mongoose.Types.ObjectId;
+  filter?: FilterQuery<unknown>;
   context: Context;
   fullAccess?: boolean;
 }
 
-function findDoc({ model, by, _id, context, fullAccess }: FindDoc) {
+function findDoc({ model, by, _id, filter, context, fullAccess }: FindDoc) {
   if (!fullAccess) requireAuthentication(context);
   const Model = mongoose.model<CollectionDoc>(model);
 
@@ -28,7 +29,7 @@ function findDoc({ model, by, _id, context, fullAccess }: FindDoc) {
         };
 
   // get the document
-  return Model.findOne({ [by || '_id']: _id || null, ...accessFilter });
+  return Model.findOne({ [by || '_id']: _id || null, ...accessFilter, ...filter });
 }
 
 export { findDoc };
--- src/config/collections/helpers/findDocAndPrune.ts
+++ src/config/collections/helpers/findDocAndPrune.ts
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+import mongoose, { FilterQuery } from 'mongoose';
 import { findDoc, pruneDocs } from '.';
 import { Context } from '../../../apollo';
 
@@ -6,6 +6,7 @@ interface FindDocsAndPrune {
   model: string;
   by?: string;
   _id: mongoose.Types.ObjectId;
+  filter?: FilterQuery<unknown>;
   context: Context;
   keep: string[];
   fullAccess?: boolean;
@@ -15,11 +16,12 @@ async function findDocAndPrune({
   model,
   by,
   _id,
+  filter,
   context,
   keep,
   fullAccess,
 }: FindDocsAndPrune): Promise<mongoose.Document> {
-  const doc = await findDoc({ model, by, _id, context, fullAccess });
+  const doc = await findDoc({ model, by, _id, filter, context, fullAccess });
 
   const prunedDoc = pruneDocs({
     input: [doc],
