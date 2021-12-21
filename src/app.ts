34fc31960449c63f0c52e2c1f0cdff3fc3511b50
Jack Buehner <github@jackbuehner.com>
2021-12-21 07:53:54 +0000

    feat: use helmet to prevent vulnerabilities

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import cookieSession from 'cookie-session';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express, { Request, Response } from 'express';
+import helmet from 'helmet';
 import passport from 'passport';
 import { apiRouter2 } from './api/v2/routes';
 import { githubWebhookHandler } from './api/v2/webhooks/github';
@@ -18,6 +19,48 @@ dotenv.config();
 
 // create express app
 const app = express();
+
+// secure app with helmet
+app.use(
+  helmet({
+    // use default content security policy
+    contentSecurityPolicy: true,
+    // sets "Cross-Origin-Embedder-Policy: require-corp"
+    // to prevent documents from loading cross-origin resources that do not
+    // explicitly grant permission
+    crossOriginEmbedderPolicy: true,
+    // sets "Cross-Origin-Opener-Policy: same-origin" to ensure that top-level
+    // documents do not share a browsing context group with cross-origin
+    // documents
+    crossOriginOpenerPolicy: true,
+    // sets "Cross-Origin-Resource-Policy: same-origin"
+    crossOriginResourcePolicy: true,
+    // allow prefetching since the server does not have external URLS to
+    // untrusted websites
+    dnsPrefetchControl: {
+      allow: true,
+    },
+    // sets the Expect-CT header, which helps mitigate misissued SSL certificates
+    expectCt: true,
+    // sets "X-Frame-Options: SAMEORIGIN"
+    frameguard: true,
+    // removes the X-Powered-By header
+    hidePoweredBy: true,
+    // prefer HTTPS over insecure HTTP
+    hsts: true,
+    // forces potentially-unsafe downloads to be saved instead of opened in browser
+    // to prevent unknown HTML from executing in site's context
+    ieNoOpen: true,
+    // sets "X-Content-Type-Options: nosniff" to prevent MIME sniffing
+    noSniff: true,
+    // sets "X-Permitted-Cross-Domain-Policies: none"
+    permittedCrossDomainPolicies: true,
+    // sets "Referrer-Policy: no-referrer"
+    referrerPolicy: true,
+    // disable xss filter, which actually causes xss vulnerabilities
+    xssFilter: true,
+  })
+);
 
 // use URL search params for query variable
 app.set('query parser', (queryString: string) => {
