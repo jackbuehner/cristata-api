eaa8e265a48dbe384fc324eb542e6934d0d01fcc
Jack Buehner <github@jackbuehner.com>
2022-02-11 23:54:13 +0000

    feat: send fathom url with pasword to admins

--- src/proxy.route.ts
+++ src/proxy.route.ts
@@ -1,6 +1,9 @@
-import { allowedOrigins } from './middleware/cors';
 import corsAnywhere from 'cors-anywhere';
+import crypto from 'crypto';
 import { Router } from 'express';
+import { Teams } from './config/database';
+import { allowedOrigins } from './middleware/cors';
+import { IDeserializedUser } from './passport';
 
 /**
  * This router contains the proxy route.
@@ -13,7 +16,7 @@ const router = Router();
 
 // create a CORS proxy
 const proxy = corsAnywhere.createServer({
-  originWhitelist: allowedOrigins, // allow all origins
+  originWhitelist: [], // allow all origins
   requireHeader: [], // don't require headers
   removeHeaders: ['cookie', 'cookie2'], // do not forward cookies
 });
@@ -22,6 +25,18 @@ const proxy = corsAnywhere.createServer({
 router.get('/proxy/*', (req, res) => {
   req.url = req.url.replace('/proxy/', '/'); // strip '/proxy' from the front of the URL (otherwise, the proxy will not work)
   proxy.emit('request', req, res);
+});
+
+// send the analytics url
+router.get('/analytics/dashboard', (req, res) => {
+  if (req.isAuthenticated() && (req.user as IDeserializedUser).teams.includes(Teams.ADMIN)) {
+    const password = crypto.createHash('sha256').update(process.env.FATHOM_DASHBOARD_PASSWORD).digest('hex'); // hash the password
+    res.json({
+      url: `https://app.usefathom.com/share/${process.env.FATHOM_SITE_ID}/wordpress?password=${password}`,
+    });
+  } else {
+    res.status(403).end();
+  }
 });
 
 export { router as proxyRouter };
