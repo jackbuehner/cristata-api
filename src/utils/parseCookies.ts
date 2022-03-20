4a09dd51fb53e78274a04bd0cf6a07ba144d9cbd
Jack Buehner <github@jackbuehner.com>
2022-03-20 17:58:04 +0000

    feat: support cookies separated by semicolon and white space

--- src/utils/parseCookies.ts
+++ src/utils/parseCookies.ts
@@ -14,49 +14,52 @@ type ParsedCookie = {
  */
 function parseCookies(cookies: string): ParsedCookie[] {
   const parsed: ParsedCookie[] = [];
-  cookies.split(';').forEach((cookie) => {
-    // destructure name and value from cookie string
-    const [name, value] = splitOnce(cookie, '=');
+  cookies
+    .replace(/; /g, ';')
+    .split(';')
+    .forEach((cookie) => {
+      // destructure name and value from cookie string
+      const [name, value] = splitOnce(cookie, '=');
 
-    // if cookie is a signature cookie, include it with the actual cookie
-    const isSig = name.includes('.sig');
-    if (isSig) {
-      // find main cookie
-      const mainIndex = parsed.findIndex((cookie) => cookie.name === name.replace('.sig', ''));
-      const mainExists = mainIndex >= 0;
+      // if cookie is a signature cookie, include it with the actual cookie
+      const isSig = name.includes('.sig');
+      if (isSig) {
+        // find main cookie
+        const mainIndex = parsed.findIndex((cookie) => cookie.name === name.replace('.sig', ''));
+        const mainExists = mainIndex >= 0;
 
-      // store in cookies array
-      if (mainExists) {
-        parsed[mainIndex] = {
-          ...parsed[mainIndex],
-          signature: value,
-        };
-      } else {
-        parsed.push({
-          name,
-          value: null,
-          signature: value,
-        });
+        // store in cookies array
+        if (mainExists) {
+          parsed[mainIndex] = {
+            ...parsed[mainIndex],
+            signature: value,
+          };
+        } else {
+          parsed.push({
+            name,
+            value: null,
+            signature: value,
+          });
+        }
       }
-    }
 
-    // otherwise, insert the main cookie
-    else {
-      // determine if main cookie already exists (due to signature being stored first)
-      const mainIndex = parsed.findIndex((cookie) => cookie.name === name);
-      const mainExists = mainIndex >= 0;
+      // otherwise, insert the main cookie
+      else {
+        // determine if main cookie already exists (due to signature being stored first)
+        const mainIndex = parsed.findIndex((cookie) => cookie.name === name);
+        const mainExists = mainIndex >= 0;
 
-      // store in cookies array
-      if (mainExists) {
-        parsed[mainIndex] = {
-          ...parsed[mainIndex],
-          value,
-        };
-      } else {
-        parsed.push({ name, value });
+        // store in cookies array
+        if (mainExists) {
+          parsed[mainIndex] = {
+            ...parsed[mainIndex],
+            value,
+          };
+        } else {
+          parsed.push({ name, value });
+        }
       }
-    }
-  });
+    });
   return parsed;
 }
 
