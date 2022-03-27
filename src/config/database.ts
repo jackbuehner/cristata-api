66384a916424b221e78aba844edbfa3097960f04
Jack Buehner <github@jackbuehner.com>
2022-03-27 00:50:36 +0000

    refactor: use json instead of ts for database collections

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,34 +1,32 @@
 import dotenv from 'dotenv';
 import { GenCollectionInput } from '../api/v3/helpers/generators/genCollection';
 import { Collection, DatabaseFunc } from '../types/config';
-import {
-  articles,
-  externalAccounts,
-  flush,
-  photoRequests,
-  photos,
-  satire,
-  settings,
-  shorturls,
-  teams,
-  users,
-} from './collections';
+import articles from './collections/articles.collection.json';
+import externalAccounts from './collections/externalAccounts.collection.json';
+import flush from './collections/flush.collection.json';
+import photoRequests from './collections/photoRequests.collection.json';
+import photos from './collections/photos.collection.json';
+import satire from './collections/satire.collection.json';
+import settings from './collections/settings.collection.json';
+import shorturls from './collections/shorturls.collection.json';
+import teams from './collections/teams.collection.json';
+import { users } from './collections/users';
 
 // load environmental variables
 dotenv.config();
 
 const database: DatabaseFunc<Collection | GenCollectionInput> = () => ({
   collections: [
+    articles as unknown as GenCollectionInput,
+    externalAccounts as unknown as GenCollectionInput,
+    flush as unknown as GenCollectionInput,
+    photoRequests as unknown as GenCollectionInput,
+    photos as unknown as GenCollectionInput,
+    satire as unknown as GenCollectionInput,
+    settings as unknown as GenCollectionInput,
+    shorturls as unknown as GenCollectionInput,
+    teams as unknown as GenCollectionInput,
     users(),
-    teams(),
-    satire(),
-    articles(),
-    shorturls(),
-    settings(),
-    photoRequests(),
-    photos(),
-    flush(),
-    externalAccounts(),
   ],
 });
 
