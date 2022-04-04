d202211fafccea3b1f6447f21302ba5930fddee9
Jack Buehner <github@jackbuehner.com>
2022-04-04 01:26:12 +0000

    fix: converting object id protype when attempring to convert null prototype

--- src/utils/convertNullPrototype.ts
+++ src/utils/convertNullPrototype.ts
@@ -1,5 +1,6 @@
 import { merge } from 'merge-anything';
 import { isObject } from './isObject';
+import { isObjectId } from './isObjectId';
 
 /**
  * Convert objects with the null prototype to the normal object prototype.
@@ -12,13 +13,13 @@ function convertNullPrototype<T extends Record<string, unknown>>(obj: T): T {
       const value = obj[key];
 
       // if it is an object of null prototype, make it an object with the normal prototype
-      if (isObject(value) && Object.getPrototypeOf(value) === null) {
+      if (isObject(value) && !isObjectId(value) && Object.getPrototypeOf(value) === null) {
         return { [key]: { ...convertNullPrototype(value) } };
       }
 
       // if it is an array, run each object in the array through this function
       if (Array.isArray(value)) {
-        return { [key]: value.filter((v) => isObject(v)).map((o) => convertNullPrototype(o)) };
+        return { [key]: value.map((o) => (isObject(o) && !isObjectId(o) ? convertNullPrototype(o) : o)) };
       }
 
       // otherwise, return the value
