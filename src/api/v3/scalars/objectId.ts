2536a17fc6b8acf121674ec0a06fb021d1b920f4
Jack Buehner <github@jackbuehner.com>
2022-04-16 22:10:25 +0000

    feat: handle instances of 0 as an objectId

--- src/api/v3/scalars/objectId.ts
+++ src/api/v3/scalars/objectId.ts
@@ -4,8 +4,10 @@ import mongoose from 'mongoose';
 const objectId = new GraphQLScalarType({
   name: 'ObjectID',
   description: 'mongoose ObjectID scalar type',
-  serialize(ObjectID: mongoose.Types.ObjectId) {
+  serialize(ObjectID: mongoose.Types.ObjectId | 0 | '0') {
     try {
+      // consider values of 0 as the team or user that represents ANY, or '000000000000000000000000'
+      if (ObjectID == 0) ObjectID = new mongoose.Types.ObjectId('000000000000000000000000');
       return new mongoose.Types.ObjectId(ObjectID).toHexString(); // Convert outgoing ObjectID to hex string
     } catch {
       return null;
