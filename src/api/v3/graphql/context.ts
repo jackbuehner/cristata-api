53d46a15af83904744e75bf1b3d179476a84c9ef
Jack Buehner <github@jackbuehner.com>
2022-07-29 18:39:01 +0000

    refactor: move context generation and type to own file

---
+++ src/api/v3/graphql/context.ts
@@ -0,0 +1,59 @@
+import { ContextFunction } from 'apollo-server-core';
+import { ExpressContext } from 'apollo-server-express';
+import mongoose from 'mongoose';
+import Cristata from '../../../Cristata';
+import { IDeserializedUser } from '../../../passport';
+import { Configuration } from '../../../types/config';
+
+interface Input extends ExpressContext {
+  __cristata: {
+    tenant: string;
+    cristata: Cristata;
+  };
+}
+
+/**
+ * Returns a context object containing authentication information.
+ */
+const context: ContextFunction<Input, Context> = ({ req, __cristata }) => {
+  const { tenant, cristata } = __cristata;
+  const config = cristata.config[tenant];
+  const restartApollo = () => cristata.restartApollo(tenant);
+
+  if (req.headers.authorization) {
+    const [type, token] = req.headers.authorization.split(' ');
+    if (type === 'app-token') {
+      const matchedToken = config.tokens?.find(({ token: appToken }) => appToken === token);
+      const isAuthenticated = !!matchedToken;
+      const profile: IDeserializedUser = {
+        _id: new mongoose.Types.ObjectId('000000000000000000000000'),
+        email: 'token@cristata.app',
+        methods: ['local'],
+        name: 'TOKEN_' + matchedToken.name,
+        next_step: '',
+        provider: 'local',
+        teams: matchedToken.scope.admin === true ? ['000000000000000000000001'] : [],
+        tenant: tenant,
+        two_factor_authentication: false,
+        username: 'TOKEN_' + matchedToken.name,
+      };
+      return { config, isAuthenticated, profile, tenant, cristata, restartApollo };
+    }
+  }
+
+  const isAuthenticated = req.isAuthenticated() && (req.user as IDeserializedUser).tenant === tenant;
+  const profile = req.user as IDeserializedUser;
+  return { config, isAuthenticated, profile, tenant, cristata, restartApollo };
+};
+
+interface Context {
+  config: Configuration;
+  isAuthenticated: boolean;
+  profile?: IDeserializedUser;
+  tenant: string;
+  cristata: Cristata;
+  restartApollo: () => Promise<Error | void>;
+}
+
+export type { Context };
+export { context };
--- src/apollo.ts
+++ src/apollo.ts
@@ -1,19 +1,17 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-import { ApolloServerPluginDrainHttpServer, ContextFunction } from 'apollo-server-core';
-import { ApolloServer as Apollo, ExpressContext } from 'apollo-server-express';
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+import { ApolloServer as Apollo } from 'apollo-server-express';
 import { Router } from 'express';
 import { execute, subscribe } from 'graphql';
 import { PubSub } from 'graphql-subscriptions';
-import mongoose from 'mongoose';
 import { SubscriptionServer } from 'subscriptions-transport-ws';
+import { context } from './api/v3/graphql/context';
 import { makeGraphSchema } from './api/v3/graphql/makeGraphSchema';
 import { CloseWebsocketServerStop, GraphQLPlayground, LogErrorsToConsole } from './api/v3/graphql/plugins';
 import { collectionResolvers } from './api/v3/resolvers';
 import { collectionTypeDefs } from './api/v3/typeDefs';
 import Cristata from './Cristata';
 import { corsConfig } from './middleware/cors';
-import { IDeserializedUser } from './passport';
-import { Configuration } from './types/config';
 
 // create publish-subscribe class for managing subscriptions
 const pubsub = new PubSub();
@@ -30,39 +28,10 @@ async function apollo(
   const wss = cristata.apolloWss[tenant];
   const config = cristata.config[tenant];
   const collections = config.collections;
-  const restartApollo = () => cristata.restartApollo(tenant);
 
   try {
     // create the base executable schema
     const schema = makeGraphSchema(collections);
-
-    // add auth info to context
-    const context: ContextFunction<ExpressContext, Context> = ({ req }) => {
-      if (req.headers.authorization) {
-        const [type, token] = req.headers.authorization.split(' ');
-        if (type === 'app-token') {
-          const matchedToken = config.tokens?.find(({ token: appToken }) => appToken === token);
-          const isAuthenticated = !!matchedToken;
-          const profile: IDeserializedUser = {
-            _id: new mongoose.Types.ObjectId('000000000000000000000000'),
-            email: 'token@cristata.app',
-            methods: ['local'],
-            name: 'TOKEN_' + matchedToken.name,
-            next_step: '',
-            provider: 'local',
-            teams: matchedToken.scope.admin === true ? ['000000000000000000000001'] : [],
-            tenant: tenant,
-            two_factor_authentication: false,
-            username: 'TOKEN_' + matchedToken.name,
-          };
-          return { config, isAuthenticated, profile, tenant, cristata, restartApollo };
-        }
-      }
-
-      const isAuthenticated = req.isAuthenticated() && (req.user as IDeserializedUser).tenant === tenant;
-      const profile = req.user as IDeserializedUser;
-      return { config, isAuthenticated, profile, tenant, cristata, restartApollo };
-    };
 
     // initialize apollo
     const apollo = new Apollo({
@@ -74,7 +43,7 @@ async function apollo(
         CloseWebsocketServerStop({ wss }),
         LogErrorsToConsole(),
       ],
-      context,
+      context: (ec) => context({ ...ec, __cristata: { cristata, tenant } }),
     });
 
     // attach subscription handler to subscriptionServer
@@ -97,17 +66,8 @@ async function apollo(
   }
 }
 
-interface Context {
-  config: Configuration;
-  isAuthenticated: boolean;
-  profile?: IDeserializedUser;
-  tenant: string;
-  cristata: Cristata;
-  restartApollo: () => Promise<Error | void>;
-}
-
 const collectionPeopleResolvers = collectionResolvers.CollectionPeople;
 const publishableCollectionPeopleResolvers = collectionResolvers.PublishableCollectionPeople;
 
-export type { Context };
+export type { Context } from './api/v3/graphql/context';
 export { apollo, collectionPeopleResolvers, collectionTypeDefs, publishableCollectionPeopleResolvers, pubsub };
