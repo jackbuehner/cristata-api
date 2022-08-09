cfcc6936a67b20b1d642e051ac8f3190dbf9a1a4
Jack Buehner <git@jackbuehner.com>
2022-08-09 14:03:17 -0400

    fix: only create text index when text fields change

--- src/api/mongodb/createTextIndex.ts
+++ src/api/mongodb/createTextIndex.ts
@@ -11,27 +11,36 @@ async function createTextIndex(
   const { textIndexFieldNames } = collection;
   const fieldNames = [...textIndexFieldNames, 'hidden'];
 
-  Schema.index(merge({}, ...fieldNames.map((fieldName) => ({ [fieldName]: 'text' }))), {
-    name: 'textIndex',
-    background: false,
-  });
+  const create = () => {
+    Schema.index(merge({}, ...fieldNames.map((fieldName) => ({ [fieldName]: 'text' }))), {
+      name: 'textIndex',
+      background: false,
+    });
+  };
 
   // if the index already exists, but the fields in the index do not match,
   // recreate the index with the correct fields
+  // OR create the index if the index does not already exist
   const currentDbCollection = tenantDB.collection(pluralize(collection.name).toLowerCase());
   // @ts-expect-error full: true is valid and has an effect
   const existingIndexes = (await currentDbCollection.getIndexes({ full: true })) || [];
   const existingTextIndex = existingIndexes.find((i) => i.name === 'textIndex');
-  if (existingTextIndex) {
+  if (existingTextIndex?.weights) {
     const existingTextIndexFields = Object.keys(existingTextIndex.weights);
     const indexesMatch =
-      existingTextIndexFields.length === textIndexFieldNames.length &&
-      textIndexFieldNames.every((fieldName) => existingTextIndexFields.includes(fieldName));
+      existingTextIndexFields.length === fieldNames.length &&
+      fieldNames.every((fieldName) => existingTextIndexFields.includes(fieldName));
     if (!indexesMatch) {
       // drop the current text index so that mongoose will recreate it when it converts
       // the schema to a model
       await currentDbCollection.dropIndex('textIndex');
+
+      // create the index
+      create();
     }
+  } else {
+    // create the index
+    create();
   }
 }
 
