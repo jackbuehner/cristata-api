9e4cdaaa3e269a95811dda3bde28aae207ae3418
Jack Buehner <git@jackbuehner.com>
2022-08-29 05:33:03 -0400

    fix: do not create yState if value is empty string

--- src/api/graphql/helpers/findDoc.ts
+++ src/api/graphql/helpers/findDoc.ts
@@ -85,7 +85,7 @@ async function findDoc({
   try {
     const uint8ToBase64 = (arr: Uint8Array): string => Buffer.from(arr).toString('base64');
 
-    if (doc?._id && !doc.yState) {
+    if (doc?._id && (doc.yState === undefined || doc.yState === null)) {
       const ydoc = new Y.Doc(); // create empty doc
       const collection = context.config.collections.find((col) => col.name === model);
       if (collection?.schemaDef) {
