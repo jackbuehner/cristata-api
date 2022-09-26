4fa24a68c7aaf4f82af15bacf095b797aa70134f
Jack Buehner <git@jackbuehner.com>
2022-09-26 02:14:37 -0400

    feat: add helper to set value in ydoc

---
+++ apps/server/src/graphql/helpers/setYDocType.ts
@@ -0,0 +1,56 @@
+import { shared } from '@jackbuehner/cristata-ydoc-utils';
+import mongoose from 'mongoose';
+import ws from 'ws';
+import { WebsocketProvider } from 'y-websocket';
+import * as Y from 'yjs';
+import { TenantDB } from '../../mongodb/TenantDB';
+import { Context } from '../server';
+
+/**
+ *
+ */
+async function setYDocType(
+  context: Context,
+  model: string,
+  id: string,
+  cb: (
+    TenantModel: (name: string) => Promise<mongoose.Model<unknown> | null>,
+    ydoc: Y.Doc,
+    sharedHelper: typeof shared
+  ) => Promise<boolean>
+): Promise<void> {
+  return await new Promise<void>((resolve, reject) => {
+    try {
+      // create an empty ydoc to use with the websocket provider
+      const ydoc = new Y.Doc();
+
+      // create a websocket provider
+      const wsProvider = new WebsocketProvider(
+        process.env.HOCUSPOCUS_SERVER || '',
+        `${context.tenant}.${model}.${id}`,
+        ydoc,
+        {
+          // @ts-expect-error it's fine
+          WebSocketPolyfill: ws,
+          params: {
+            authSecret: process.env.AUTH_OVERRIDE_SECRET || '',
+          },
+        }
+      );
+
+      // run the callback once a sync event has happened
+      wsProvider.once('sync', async () => {
+        // connect to the database
+        const tenantDB = new TenantDB(context.tenant, context.config.collections);
+        await tenantDB.connect();
+
+        // execute the callback
+        if (await cb(tenantDB.model.bind(tenantDB), ydoc, shared)) resolve();
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+export { setYDocType };
