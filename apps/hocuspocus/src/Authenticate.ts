50dd24f31ec3ee1568062f4472f1053b395d2f5f
Jack Buehner <git@jackbuehner.com>
2022-09-27 20:12:45 -0400

    feat: check auth read/write ability before connecting

--- apps/hocuspocus/src/Authenticate.ts
+++ apps/hocuspocus/src/Authenticate.ts
@@ -1,8 +1,11 @@
+import { Forbidden, Unauthorized } from '@hocuspocus/common';
 import { Extension, onConnectPayload } from '@hocuspocus/server';
+import { uncapitalize } from '@jackbuehner/cristata-utils';
 import fetch from 'node-fetch';
 
 export interface AuthenticateConfiguration {
   authHref: string;
+  apiEndpoint: string;
 }
 
 class Authenticate implements Extension {
@@ -12,7 +15,14 @@ class Authenticate implements Extension {
     this.configuration = configuration;
   }
 
-  async onConnect({ connection, requestHeaders, requestParameters }: onConnectPayload): Promise<void> {
+  async onConnect({
+    connection,
+    requestHeaders,
+    requestParameters,
+    documentName,
+  }: onConnectPayload): Promise<void> {
+    const [tenant, collectionName, itemId] = documentName.split('.');
+
     // always start off as unauthenticated
     connection.requiresAuthentication = true;
     connection.isAuthenticated = false;
@@ -25,8 +35,9 @@ class Authenticate implements Extension {
       connection.isAuthenticated = true;
     }
 
-    // check authentication status
+    // confirm the document can be read and edited by the client
     if (!connection.isAuthenticated) {
+      // check authentication status
       const authRes = await fetch(this.configuration.authHref, {
         method: 'GET',
         headers: {
@@ -35,11 +46,39 @@ class Authenticate implements Extension {
         },
       });
 
-      if (authRes.status === 200) {
+      if (authRes.status !== 200) {
+        // prevent later hooks and default handler
+        throw Unauthorized;
+      }
+
+      // check read/write access
+      const accessRes = await fetch(`${this.configuration.apiEndpoint}/${tenant}`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', cookie: requestHeaders.cookie || '' },
+        body: JSON.stringify({
+          query: `
+            query GetAccess($id: ObjectID) {
+              ${uncapitalize(collectionName)}ActionAccess(_id: $id) {
+                get
+                modify
+              }
+            }
+          `,
+          variables: { id: itemId },
+        }),
+      });
+
+      if (!accessRes.ok) {
+        throw new Error('failed to query for read/write access');
+      }
+
+      const json = await accessRes.json();
+      const access = json?.data?.[`${uncapitalize(collectionName)}ActionAccess`];
+      if (access?.get && access?.modify) {
         connection.isAuthenticated = true;
       } else {
         // prevent later hooks and default handler
-        throw new Error();
+        throw Forbidden;
       }
     }
   }
--- apps/hocuspocus/src/index.ts
+++ apps/hocuspocus/src/index.ts
@@ -2,8 +2,17 @@ import { Hocuspocus } from '@hocuspocus/server';
 import { Authenticate } from './Authenticate';
 import { db } from './extension-db';
 
+if (!process.env.SERVER_AUTH_URL) {
+  throw new Error('SERVER_AUTH_URL is a required environment variable');
+}
+
+if (!process.env.SERVER_API_URL) {
+  throw new Error('SERVER_API_URL is a required environment variable');
+}
+
 const authentication = new Authenticate({
-  authHref: process.env.SERVER_AUTH_URL || '',
+  authHref: process.env.SERVER_AUTH_URL,
+  apiEndpoint: process.env.SERVER_API_URL,
 });
 
 const hocuspocus = new Hocuspocus({
