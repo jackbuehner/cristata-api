eb0b62c4a147c2bb4a306fb808bd9eecfe3ff0b7
Jack Buehner <git@jackbuehner.com>
2022-10-11 18:29:13 -0400

    fix: move sort operator to end of query to resolve memory issue

--- apps/server/src/graphql/helpers/findDocs.ts
+++ apps/server/src/graphql/helpers/findDocs.ts
@@ -94,9 +94,9 @@ async function findDocs({ model, args, context, fullAccess, accessRule }: FindDo
     { $match: filter ? filter : {} },
     { $match: accessFilter },
     { $match: _ids ? { _id: { $in: _ids } } : {} },
-    { $sort: { 'timestamps.created_at': -1 } },
-    ...(args.pipeline2 || []),
     { $project: { __yState: 0, __yVersions: 0, yState: 0, __migrationBackup: 0 } },
+    ...(args.pipeline2 || []),
+    { $sort: { 'timestamps.created_at': -1 } },
   ];
 
   const aggregate = Model.aggregate(pipeline);
