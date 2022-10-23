c83c55e742c625e380a082c4ce67a3fde290d8bb
Jack Buehner <git@jackbuehner.com>
2022-10-23 02:30:12 -0400

    refactor: add better types of people resolvers

--- apps/server/src/graphql/resolvers/collection.ts
+++ apps/server/src/graphql/resolvers/collection.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import getFieldNames from 'graphql-list-fields';
+import { IUserDoc } from '../../mongodb/users';
 import mongoose from 'mongoose';
 import { getUsers } from '../helpers';
 import { Context } from '../server';
@@ -7,31 +8,28 @@ import { Context } from '../server';
 type ObjectId = mongoose.Types.ObjectId;
 type Info = Parameters<typeof getFieldNames>[0];
 
-const collectionPeopleResolvers = {
-  created_by: ({ created_by }: { created_by: ObjectId }, __: never, context: Context, info: Info) =>
-    getUsers(created_by, context, info),
-  modified_by: ({ modified_by }: { modified_by: ObjectId }, __: never, context: Context, info: Info) =>
-    getUsers(modified_by, context, info),
-  last_modified_by: (
-    { last_modified_by }: { last_modified_by: ObjectId },
-    __: never,
+type PeopleResolverGroup = Record<
+  string,
+  (
+    parent: Record<string, unknown>,
+    args: never,
     context: Context,
     info: Info
-  ) => getUsers(last_modified_by, context, info),
-  watching: ({ watching }: { watching: ObjectId }, __: never, context: Context, info: Info) =>
-    getUsers(watching, context, info),
+  ) => Promise<IUserDoc | IUserDoc[] | null>
+>;
+
+const collectionPeopleResolvers: PeopleResolverGroup = {
+  created_by: (parent, __, context, info) => getUsers(parent.created_by as ObjectId, context, info),
+  modified_by: (parent, __, context, info) => getUsers(parent.modified_by as ObjectId, context, info),
+  last_modified_by: (parent, __, context, info) => getUsers(parent.last_modified_by as ObjectId, context, info),
+  watching: (parent, __, context, info) => getUsers(parent.watching as ObjectId, context, info),
 };
 
-const publishableCollectionPeopleResolvers = {
+const publishableCollectionPeopleResolvers: PeopleResolverGroup = {
   ...collectionPeopleResolvers,
-  published_by: ({ published_by }: { published_by: ObjectId }, __: never, context: Context, info: Info) =>
-    getUsers(published_by, context, info),
-  last_published_by: (
-    { last_published_by }: { last_published_by: ObjectId },
-    __: never,
-    context: Context,
-    info: Info
-  ) => getUsers(last_published_by, context, info),
+  published_by: (parent, __, context, info) => getUsers(parent.published_by as ObjectId, context, info),
+  last_published_by: (parent, __, context, info) =>
+    getUsers(parent.last_published_by as ObjectId, context, info),
 };
 
 const collection = {
@@ -44,3 +42,4 @@ const collection = {
 };
 
 export { collection };
+export type { PeopleResolverGroup };
