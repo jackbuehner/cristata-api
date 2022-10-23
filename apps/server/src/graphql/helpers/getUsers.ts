83c13100b68c3ed026b3aa2317bb4963b86e5966
Jack Buehner <git@jackbuehner.com>
2022-10-23 02:12:24 -0400

    feat: use projection when getting users

--- apps/server/src/graphql/helpers/getUsers.ts
+++ apps/server/src/graphql/helpers/getUsers.ts
@@ -1,17 +1,29 @@
 import { notEmpty } from '@jackbuehner/cristata-utils';
+import getFieldNames from 'graphql-list-fields';
 import mongoose from 'mongoose';
 import { TenantDB } from '../../mongodb/TenantDB';
 import { IUser, IUserDoc } from '../../mongodb/users';
 import { Context } from '../server';
 
+type GraphQLResolveInfo = Parameters<typeof getFieldNames>[0];
+
 /**
  * Get the user or array of users from one or more unique user object IDs.
  */
-async function getUsers(_ids: mongoose.Types.ObjectId, context: Context): Promise<IUserDoc | null>;
-async function getUsers(_ids: mongoose.Types.ObjectId[], context: Context): Promise<IUserDoc[]>;
+async function getUsers(
+  _ids: mongoose.Types.ObjectId,
+  context: Context,
+  info?: GraphQLResolveInfo | string[]
+): Promise<IUserDoc | null>;
+async function getUsers(
+  _ids: mongoose.Types.ObjectId[],
+  context: Context,
+  info?: GraphQLResolveInfo | string[]
+): Promise<IUserDoc[]>;
 async function getUsers(
   _ids: mongoose.Types.ObjectId | mongoose.Types.ObjectId[],
-  context: Context
+  context: Context,
+  info?: GraphQLResolveInfo | string[]
 ): Promise<IUserDoc | IUserDoc[] | null> {
   const tenantDB = new TenantDB(context.tenant, context.config.collections);
   await tenantDB.connect();
@@ -19,15 +31,44 @@ async function getUsers(
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   const User = (await tenantDB.model<IUser>('User'))!;
 
+  const [projection, fields] = createProjection(info);
+  const isOnlyId = fields.length === 1 && fields[0] === '_id';
+
   // if it is undefined
   if (!_ids) return [];
+
+  // if the client only wants the _id field, we do not need to query the database
+  if (isOnlyId) {
+    if (Array.isArray(_ids)) return _ids.map((_id) => ({ _id } as IUserDoc));
+    return { _id: _ids } as IUserDoc;
+  }
+
   // if it is an array of ObjectId
   if (Array.isArray(_ids)) {
-    return (await Promise.all(_ids.map(async (_id) => await User.findById(_id)))).filter(notEmpty);
+    return (await Promise.all(_ids.map(async (_id) => await User.findById(_id, projection)))).filter(notEmpty);
   }
+
   // if it just a single ObjectId
   const _id = _ids;
-  return await User.findById(_id);
+  return await User.findById(_id, projection);
+}
+
+/**
+ * Creates a projection that only fetches the document fields
+ * requested in the GraphQL query.
+ */
+function createProjection(info?: GraphQLResolveInfo | string[]): [Record<string, 1> | undefined, string[]] {
+  if (!info) return [undefined, []];
+
+  // get the names of the requested fields
+  const fields = Array.isArray(info) ? info : getFieldNames(info).map((field) => field.replace('docs.', ''));
+
+  const projection: Record<string, 1> = {};
+  fields.forEach((field) => {
+    projection[field] = 1;
+  });
+
+  return [projection, fields];
 }
 
 export { getUsers };
--- apps/server/src/graphql/resolvers/collection.ts
+++ apps/server/src/graphql/resolvers/collection.ts
@@ -1,38 +1,45 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-import { Context } from '../server';
-import { getUsers } from '../helpers';
+import getFieldNames from 'graphql-list-fields';
 import mongoose from 'mongoose';
+import { getUsers } from '../helpers';
+import { Context } from '../server';
+
+type ObjectId = mongoose.Types.ObjectId;
+type Info = Parameters<typeof getFieldNames>[0];
 
 const collectionPeopleResolvers = {
-  created_by: ({ created_by }: { created_by: mongoose.Types.ObjectId }, __: never, context: Context) =>
-    getUsers(created_by, context),
-  modified_by: ({ modified_by }: { modified_by: mongoose.Types.ObjectId }, __: never, context: Context) =>
-    getUsers(modified_by, context),
+  created_by: ({ created_by }: { created_by: ObjectId }, __: never, context: Context, info: Info) =>
+    getUsers(created_by, context, info),
+  modified_by: ({ modified_by }: { modified_by: ObjectId }, __: never, context: Context, info: Info) =>
+    getUsers(modified_by, context, info),
   last_modified_by: (
-    { last_modified_by }: { last_modified_by: mongoose.Types.ObjectId },
+    { last_modified_by }: { last_modified_by: ObjectId },
     __: never,
-    context: Context
-  ) => getUsers(last_modified_by, context),
-  watching: ({ watching }: { watching: mongoose.Types.ObjectId }, __: never, context: Context) =>
-    getUsers(watching, context),
+    context: Context,
+    info: Info
+  ) => getUsers(last_modified_by, context, info),
+  watching: ({ watching }: { watching: ObjectId }, __: never, context: Context, info: Info) =>
+    getUsers(watching, context, info),
 };
 
 const publishableCollectionPeopleResolvers = {
   ...collectionPeopleResolvers,
-  published_by: ({ published_by }: { published_by: mongoose.Types.ObjectId }, __: never, context: Context) =>
-    getUsers(published_by, context),
+  published_by: ({ published_by }: { published_by: ObjectId }, __: never, context: Context, info: Info) =>
+    getUsers(published_by, context, info),
   last_published_by: (
-    { last_published_by }: { last_published_by: mongoose.Types.ObjectId },
+    { last_published_by }: { last_published_by: ObjectId },
     __: never,
-    context: Context
-  ) => getUsers(last_published_by, context),
+    context: Context,
+    info: Info
+  ) => getUsers(last_published_by, context, info),
 };
 
 const collection = {
   CollectionPeople: collectionPeopleResolvers,
   PublishableCollectionPeople: publishableCollectionPeopleResolvers,
   CollectionHistory: {
-    user: ({ user }: { user: mongoose.Types.ObjectId }, __: never, context: Context) => getUsers(user, context),
+    user: ({ user }: { user: ObjectId }, __: never, context: Context, info: Info) =>
+      getUsers(user, context, info),
   },
 };
 
