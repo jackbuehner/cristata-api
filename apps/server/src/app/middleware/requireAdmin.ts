22c588c97c4e47d121d3701e86366c1b9771e95e
Jack Buehner <git@jackbuehner.com>
2022-12-30 04:22:42 -0500

    fix: use proper auth checks

--- apps/server/src/app/middleware/requireAdmin.ts
+++ apps/server/src/app/middleware/requireAdmin.ts
@@ -1,17 +1,45 @@
-import { Request, Response, NextFunction } from 'express';
+import { NextFunction, Request, Response } from 'express';
+import mongoose from 'mongoose';
 import { IDeserializedUser } from '../passport';
 
 /**
  * Middleware to be use on specific routes to require authentication.
- * If `req.user` is defined and `req.user.teams` includes the admin
- * team id ('000000000000000000000001'), go to next step.
+ * If `req.user` is defined and the users `teams` array in the database
+ * includes the admin team id ('000000000000000000000001'), go to next step.
  * Otherwise, return HTTP error to 403 to the client.
  */
-const requireAdmin = (req: Request, res: Response, next: NextFunction): void => {
+const requireAdmin = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   const isAuthenticated = req.isAuthenticated();
-  const isAdmin = (req.user as IDeserializedUser).teams.includes('000000000000000000000001');
+  if (!isAuthenticated) {
+    res.status(401).end();
+    return;
+  }
 
-  if (isAuthenticated && isAdmin) {
+  const user = req.user as IDeserializedUser;
+  const tenant = user.tenant;
+
+  const conn = global.mongoose?.app?.conn?.useDb(tenant);
+  if (!conn) {
+    res.status(500).end();
+    return;
+  }
+
+  const userTeams = await conn.db
+    .collection('teams')
+    .find(
+      {
+        $or: [
+          { organizers: new mongoose.Types.ObjectId(user._id) },
+          { members: new mongoose.Types.ObjectId(user._id) },
+        ],
+      },
+      { projection: { _id: 1 } }
+    )
+    .toArray();
+
+  const isAdmin = userTeams?.map((team) => team._id.toHexString()).includes('000000000000000000000001');
+
+  if (isAdmin) {
     next();
   } else {
     res.status(403).end();
