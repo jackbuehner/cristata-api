b5819f4a96848dcb7e47f978065347362f39d4e4
Jack Buehner <git@jackbuehner.com>
2023-08-24 00:06:42 -0400

    feat: log warning stacks

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -2,3 +2,9 @@ import CristataServer from './Cristata';
 
 const server = new CristataServer();
 server.start();
+
+// log the stack of warnings so we can figure out from where the warning is coming
+process.on('warning', (error) => {
+  console.warn(error);
+  console.warn(error.stack);
+});
