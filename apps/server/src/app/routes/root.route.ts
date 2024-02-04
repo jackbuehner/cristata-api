42f2792eb38eb2f6c8a568528ef3151603d915d1
Jack Buehner <github@jackbuehner.com>
2024-02-04 17:48:08 -0500

    fix: use correct parameter speilling

--- apps/server/src/app/routes/root.route.ts
+++ apps/server/src/app/routes/root.route.ts
@@ -1,12 +1,12 @@
-import ColorHash from 'color-hash';
-import { Router } from 'express';
-import https from 'https';
-import mime from 'mime';
-import { TenantDB } from '../../mongodb/TenantDB';
-import { IFile } from '../../mongodb/files';
-import { IPhoto } from '../../mongodb/photos';
-import { IUser } from '../../mongodb/users';
-import { IDeserializedUser } from '../passport';
+import ColorHash from "color-hash";
+import { Router } from "express";
+import https from "https";
+import mime from "mime";
+import { TenantDB } from "../../mongodb/TenantDB";
+import { IFile } from "../../mongodb/files";
+import { IPhoto } from "../../mongodb/photos";
+import { IUser } from "../../mongodb/users";
+import { IDeserializedUser } from "../passport";
 
 /**
  * Router for root endpoints for the v3 API.
@@ -14,18 +14,19 @@ import { IDeserializedUser } from '../passport';
  */
 const router = Router();
 
-router.get('/v3/:tenant/user-photo/:user_id', async (req, res) => {
+router.get("/v3/:tenant/user-photo/:user_id", async (req, res) => {
   try {
     // connect to database
     const tenantDB = new TenantDB(req.params.tenant);
     await tenantDB.connect();
-    const User = await tenantDB.model<IUser>('User');
+    const User = await tenantDB.model<IUser>("User");
 
     // make authenticated user available
     const authUser = req.user as IDeserializedUser;
 
     // identify the user _id
-    const user_id = req.params.user_id === 'me' ? authUser._id : req.params.user_id;
+    const user_id =
+      req.params.user_id === "me" ? authUser._id : req.params.user_id;
 
     // get the user
     const user = await User?.findById(user_id, { _id: 1, photo: 1 });
@@ -33,14 +34,17 @@ router.get('/v3/:tenant/user-photo/:user_id', async (req, res) => {
     // set the header for the phooto type
     if (user?.photo) {
       const type = mime.lookup(user.photo);
-      const charset = mime.charsets.lookup(user.photo, 'UTF-8');
-      res.setHeader('Content-Type', type + (charset ? '; charset=' + charset : ''));
+      const charset = mime.charsets.lookup(user.photo, "UTF-8");
+      res.setHeader(
+        "Content-Type",
+        type + (charset ? "; charset=" + charset : "")
+      );
     } else {
-      res.setHeader('Content-Type', 'image/svg+xml');
+      res.setHeader("Content-Type", "image/svg+xml");
     }
 
     // tell clients to cache profile photos for 5 minutes
-    res.set('Cache-control', 'public, max-age=300');
+    res.set("Cache-control", "public, max-age=300");
 
     // pipe the request to the user photo url
     if (user?.photo) {
@@ -49,22 +53,29 @@ router.get('/v3/:tenant/user-photo/:user_id', async (req, res) => {
       });
       externalReq.end();
     } else if (user) {
-      const size = (req.query as unknown as URLSearchParams).get('size') || '120';
+      const size =
+        (req.query as unknown as URLSearchParams).get("size") || "120";
 
-      const avatar = new URL(`https://source.boringavatars.com/beam/${size}/62fd3aef937c1ea557370d33`);
-      avatar.searchParams.set('square', '');
+      const avatar = new URL(
+        `https://source.boringavatars.com/beam/${size}/62fd3aef937c1ea557370d33`
+      );
+      avatar.searchParams.set("square", "");
 
       // @ts-expect-error 'bkdr' is a vlid hash config value
-      const colorHash = new ColorHash({ saturation: 0.6, lightness: 0.7, hash: 'bkdr' }); // note that this config is different than the one that picks the user accent color
+      const colorHash = new ColorHash({
+        saturation: 0.6,
+        lightness: 0.7,
+        hash: "bkdr",
+      }); // note that this config is different than the one that picks the user accent color
 
       const id = user._id.toHexString();
       const colors = [
         colorHash.hex(id),
         colorHash.hex(id.substr(4, id.length / 2)),
         colorHash.hex(id.substr(id.length / 2, id.length)),
-        colorHash.hex(id.split('').reverse().join('')),
+        colorHash.hex(id.split("").reverse().join("")),
       ];
-      avatar.searchParams.set('colors', colors.toString().replace(/#/g, ''));
+      avatar.searchParams.set("colors", colors.toString().replace(/#/g, ""));
 
       const externalReq = https.request(avatar.toString(), (externalRes) => {
         externalRes.pipe(res);
@@ -81,16 +92,19 @@ router.get('/v3/:tenant/user-photo/:user_id', async (req, res) => {
 
 // always redirect troop-370 file urls since they no longer use Cristata
 // but they want the old filestore urls to still work
-router.get('/filestore/troop-370/:id', (req, res) => {
-  res.redirect(301, `https://troop370atlanta.org/cristata-filestore/${req.params._id}`)
-})
+router.get("/filestore/troop-370/:_id", (req, res) => {
+  res.redirect(
+    301,
+    `https://troop370atlanta.org/cristata-filestore/${req.params._id}`
+  );
+});
 
-router.get('/filestore/:tenant/:_id', async (req, res) => {
+router.get("/filestore/:tenant/:_id", async (req, res) => {
   try {
     // connect to database
     const tenantDB = new TenantDB(req.params.tenant);
     await tenantDB.connect();
-    const File = await tenantDB.model<IFile>('File');
+    const File = await tenantDB.model<IFile>("File");
 
     // get the file, and end the request if it cannot be found
     const foundFile = await File?.findById(req.params._id);
@@ -107,12 +121,17 @@ router.get('/filestore/:tenant/:_id', async (req, res) => {
 
     // use the correct mime type and name
     const fileName =
-      (foundFile.name.split('.').slice(0, -1) || 'file') + '.' + mime.extension(foundFile.file_type);
-    res.setHeader('Content-Type', `${foundFile.file_type}; charset=UTF-8`);
-    res.setHeader('Content-Disposition', `inline; filename="${fileName.replace(/[^a-zA-Z-._\d\s:]/g, '_')}"`);
+      (foundFile.name.split(".").slice(0, -1) || "file") +
+      "." +
+      mime.extension(foundFile.file_type);
+    res.setHeader("Content-Type", `${foundFile.file_type}; charset=UTF-8`);
+    res.setHeader(
+      "Content-Disposition",
+      `inline; filename="${fileName.replace(/[^a-zA-Z-._\d\s:]/g, "_")}"`
+    );
 
     // allow usage on non-Cristata websites
-    res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
+    res.setHeader("Cross-Origin-Resource-Policy", "cross-origin");
 
     // pipe the request to this url
     const s3href = `https://s3.us-east-1.amazonaws.com/app.cristata.${req.params.tenant}.files/${foundFile.uuid}`;
@@ -126,12 +145,12 @@ router.get('/filestore/:tenant/:_id', async (req, res) => {
   }
 });
 
-router.get('/photo/:tenant/:_id', async (req, res) => {
+router.get("/photo/:tenant/:_id", async (req, res) => {
   try {
     // connect to database
     const tenantDB = new TenantDB(req.params.tenant);
     await tenantDB.connect();
-    const Photo = await tenantDB.model<IPhoto>('Photo');
+    const Photo = await tenantDB.model<IPhoto>("Photo");
 
     // get the photo, and end the request if it cannot be found
     const foundPhoto = await Photo?.findById(req.params._id);
@@ -148,17 +167,22 @@ router.get('/photo/:tenant/:_id', async (req, res) => {
 
     // use the correct mime type and name
     const fileName =
-      (foundPhoto.name.split('.').slice(0, -1) || 'photo') + '.' + mime.extension(foundPhoto.file_type);
-    res.setHeader('Content-Type', `${foundPhoto.file_type}; charset=UTF-8`);
-    res.setHeader('Content-Disposition', `inline; filename="${fileName.replace(/[^a-zA-Z-._\d\s:]/g, '_')}"`);
+      (foundPhoto.name.split(".").slice(0, -1) || "photo") +
+      "." +
+      mime.extension(foundPhoto.file_type);
+    res.setHeader("Content-Type", `${foundPhoto.file_type}; charset=UTF-8`);
+    res.setHeader(
+      "Content-Disposition",
+      `inline; filename="${fileName.replace(/[^a-zA-Z-._\d\s:]/g, "_")}"`
+    );
 
     // cache for a year -- photos are immutable
-    res.setHeader('Cache-Control', 'private, max-age=31536000');
+    res.setHeader("Cache-Control", "private, max-age=31536000");
 
     // determine the bucket for this tenant
     const bucketName =
-      req.params.tenant === 'paladin-news'
-        ? 'paladin-photo-library'
+      req.params.tenant === "paladin-news"
+        ? "paladin-photo-library"
         : `app.cristata.${req.params.tenant}.photos`;
 
     // construct the correct URL for this photo
@@ -166,13 +190,19 @@ router.get('/photo/:tenant/:_id', async (req, res) => {
     // and fall back to the original photo location
     let s3href: string;
     const searchParams = req.query as unknown as URLSearchParams;
-    if (searchParams.has('tr')) {
-      const transformations = (searchParams.get('tr') || '').split(',');
-      const width = transformations.find((tr) => tr.indexOf('w-') === 0)?.replace('w-', '');
-      const height = transformations.find((tr) => tr.indexOf('h-') === 0)?.replace('h-', '');
-      const resizeFit = transformations.find((tr) => tr.indexOf('fit-') === 0)?.replace('fit-', '');
+    if (searchParams.has("tr")) {
+      const transformations = (searchParams.get("tr") || "").split(",");
+      const width = transformations
+        .find((tr) => tr.indexOf("w-") === 0)
+        ?.replace("w-", "");
+      const height = transformations
+        .find((tr) => tr.indexOf("h-") === 0)
+        ?.replace("h-", "");
+      const resizeFit = transformations
+        .find((tr) => tr.indexOf("fit-") === 0)
+        ?.replace("fit-", "");
 
-      const cloudFrontLocation = 'https://dsm94u2p4guhh.cloudfront.net';
+      const cloudFrontLocation = "https://dsm94u2p4guhh.cloudfront.net";
 
       const imageParams = {
         bucket: bucketName,
@@ -189,12 +219,12 @@ router.get('/photo/:tenant/:_id', async (req, res) => {
       s3href = `${cloudFrontLocation}/${btoa(JSON.stringify(imageParams))}`;
 
       // allow usage only on Cristata websites
-      res.setHeader('Cross-Origin-Resource-Policy', 'same-site');
+      res.setHeader("Cross-Origin-Resource-Policy", "same-site");
     } else {
       s3href = `https://s3.us-east-1.amazonaws.com/${bucketName}/${foundPhoto.uuid}`;
 
       // allow usage on non-Cristata websites
-      res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
+      res.setHeader("Cross-Origin-Resource-Policy", "cross-origin");
     }
 
     // pipe the request to this photo
