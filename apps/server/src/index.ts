15b8e321735cccb5ccac0fd7865b24bc33d7cc0f
Jack Buehner <github@jackbuehner.com>
2024-04-07 01:39:39 -0400

    feat!: switch to stub server

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,10 +1,20 @@
-import CristataServer from './Cristata';
+import cors from 'cors';
+import express from 'express';
 
-const server = new CristataServer();
-server.start();
+if (!process.env.PORT) throw new Error('PORT not defined in env');
 
-// log the stack of warnings so we can figure out from where the warning is coming
-process.on('warning', (error) => {
-  console.warn(error);
-  console.warn(error.stack);
+// create express app
+const app = express();
+
+// enable CORS for the app
+app.use(cors({ origin: true }));
+
+// always redirect troop-370 file urls since they no longer use Cristata
+// but they want the old filestore urls to still work
+app.get('/filestore/troop-370/:_id', (req, res) => {
+  res.redirect(301, `https://troop370atlanta.org/cristata-filestore/${req.params._id}`);
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Cristata server stub listening on port ${process.env.PORT}!`);
 });
